8d428440b3a8302e32dbe0c99799aac2de9bc294
Jacques <jacques.weber@noos.fr>
2022-08-18 10:46:15 +0200

    contract helper test added

--- libs/oracle/src/lib/contract/__tests__/helpers.test.ts
+++ libs/oracle/src/lib/contract/__tests__/helpers.test.ts
@@ -1,3 +1,7 @@
-it('TODO: implem', () => {
-  expect(true).toEqual(true);
+import { toTimestamp } from "../helpers";
+
+it('toTimestamp', () => {
+  const dateString: string = "Thu Aug 18 2022 10:40:10";
+  const timestamp: number = toTimestamp(dateString);
+  expect(timestamp).toEqual(1660812010000);
 });
