2fd8e0e8f70ac3d1378a4a5d706f8e9c204fe2b6
Sylvain Bossut <sylvain.bossut@neofacto.com>
2022-09-22 11:59:33 +0200

    docs: update docker compose example in setup steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ PRJT_ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P | grep -o 
 # Install rush if needed
 if ! command -v rush > /dev/null 2>&1
 then
-    npm i -g rush
+    npm i -g @microsoft/rush
 fi
 
 rush update
