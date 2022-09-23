823009092b4e3c0738f17d0313e8ef9cb79612a1
Sylvain Bossut <sylvain.bossut@neofacto.com>
2022-09-23 17:17:40 +0200

    chore: stop nestjs from listening a port

--- apps/bootstrap-node/src/main.ts
+++ apps/bootstrap-node/src/main.ts
@@ -1,22 +1,10 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module.js';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule.forRoot(), {
+  await NestFactory.createApplicationContext(AppModule.forRoot(), {
     logger: false
   });
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 await bootstrap();
