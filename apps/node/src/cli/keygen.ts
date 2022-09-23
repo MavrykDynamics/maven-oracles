c77ccdf5e9b5698fd8a6d6eea34a2173e872c0e3
Sylvain Bossut <sylvain.bossut@neofacto.com>
2022-09-23 15:31:17 +0200

    fix: eslint

--- apps/node/src/cli/keygen.ts
+++ apps/node/src/cli/keygen.ts
@@ -1,6 +1,6 @@
 import { createSecp256k1PeerId } from '@libp2p/peer-id-factory';
 
-async function keygen() {
+async function keygen(): Promise<void> {
   const peerId = await createSecp256k1PeerId();
 
   console.log(`P2P_PEER_ID=${peerId}`);
@@ -8,4 +8,5 @@ async function keygen() {
   console.log(`P2P_PEER_PRIVATE_KEY=${Buffer.from(peerId.privateKey!).toString('base64')}`);
 }
 
-keygen();
+// eslint-disable-next-line no-void
+void keygen();
